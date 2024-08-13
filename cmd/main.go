175d644a435de8aa8390b96797b5eee595c78eea
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-13 07:53:46 +0330

    feat(main) add main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,68 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/mohrezfadaei/marzban-node-go/internal/certificate"
+	"github.com/mohrezfadaei/marzban-node-go/internal/config"
+	"github.com/mohrezfadaei/marzban-node-go/internal/logger"
+	"github.com/mohrezfadaei/marzban-node-go/internal/services"
+	"github.com/mohrezfadaei/marzban-node-go/internal/xray"
+)
+
+func generateSSLFiles(certFile, keyFile string) {
+	certPem, keyPem, err := certificate.GenerateCertificate()
+	if err != nil {
+		log.Fatalf("Failed to generate certificate: %v", err)
+	}
+
+	if err := os.WriteFile(certFile, []byte(certPem), 0644); err != nil {
+		log.Fatalf("Failed to write certificate file: %v", err)
+	}
+
+	if err := os.WriteFile(keyFile, []byte(keyPem), 0644); err != nil {
+		log.Fatalf("Failed to write key file: %v", err)
+	}
+}
+
+func main() {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	if _, err := os.Stat(conf.SslCertFile); os.IsNotExist(err) || os.IsNotExist(err) {
+		generateSSLFiles(conf.SslCertFile, conf.SslKeyFile)
+	}
+
+	if conf.SslClientCertFile == "" {
+		logger.Warn.Println("You are running node without SSL_CLIENT_CERT_FILE, be aware that everyone can connect to this node and this isn't secure!")
+	}
+
+	if conf.SslClientCertFile != "" && !fileExists(conf.SslClientCertFile) {
+		logger.Error.Println("Client's certificate file specified on SSL_CLIENT_CERT_FILE is missing")
+		os.Exit(1)
+	}
+
+	core := xray.NewXRayCore(conf.XrayExecutablePath, conf.XrayAssetsPath)
+
+	switch conf.ServiceProtocol {
+	case "rpyc":
+		if err := services.RunGRPCServer(conf, core); err != nil {
+			logger.Error.Printf("Failed to run gRPC server: %v", err)
+		}
+	case "rest":
+		if err := services.RunRestServer(conf, core); err != nil {
+			logger.Error.Printf("Failed to run REST server: %v", err)
+		}
+	default:
+		logger.Error.Println("SERVICE_PROTOCOL is not any of (rpyc, rest).")
+		os.Exit(1)
+	}
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	return err == nil && !info.IsDir()
+}
